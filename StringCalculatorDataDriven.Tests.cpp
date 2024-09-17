b3484dfabdf7a181fb162b5ba83672d9dbbbf4e4
AshaMuskan <singavarmmuskan@gmail.com>
2024-09-17 17:33:54 +0530

    Update StringCalculatorDataDriven.Tests.cpp

--- StringCalculatorDataDriven.Tests.cpp
+++ StringCalculatorDataDriven.Tests.cpp
@@ -7,7 +7,7 @@ class TestDataPair{
 public:
   string input;
   int expectedValue;
-  TestDataPair(string_input,int_expectedValue) : input{_input},expectedValue{_expectedValue}{
+  TestDataPair(string_input,int _expectedValue) : input{_input},expectedValue{_expectedValue}{
 }
 };
  
@@ -19,11 +19,11 @@ TestDataPair pair_three {"1",1};
 TestDataPair pair_four {"1,2",3};
 TestDataPair pair_five {"1,2,3",6};
 
-dataList.pushback(pair_one);
-dataList.pushback(pair_two);
-dataList.pushback(pair_three);
-dataList.pushback(pair_four);
-dataList.pushback(pair_five);
+dataList.push_back(pair_one);
+dataList.push_back(pair_two);
+dataList.push_back(pair_three);
+dataList.push_back(pair_four);
+dataList.push_back(pair_five);
 
 //iterate using a range-based for -loop
   for(TestDataPair datapair : datalist) {
