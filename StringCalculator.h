6a2eff8888665aa068f36e6c393facde7dff0107
AshaMuskan <singavarmmuskan@gmail.com>
2024-09-18 14:34:49 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,5 +4,6 @@
 #include <numeric>
 #include <regex>
 using namespace std;
-void Add(const string &input);
+//void Add(const string &input);
 //int Add(const string &input);
+void  Add(const string &input,void(*displayPtr)(int));
